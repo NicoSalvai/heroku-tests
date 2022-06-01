5de1718c585c7632d143e30eeeb7e91ed55632f1
Nicolas Salvai <nicolas-salvai@hotmail.com>
2022-06-01 14:56:26 -0300

    clean up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import './App.css';
-import Navbar from './components/navbar/Navbar';
-import LandingPage from './components/landing-page/LandingPage';
-import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <LandingPage/>
-      <Footer/>
     </div>
   );
 }
